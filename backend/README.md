08099502e5d7eb7cbadbcc86e856582939ecd273
lfuegner <lfuegner@gevb.de>
2023-10-29 13:27:49 +0100

    Feat: gitignore, pycache

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,8 @@
 # Install all Python packages
 pip install -r requirements.txt 
+
+
+# Opensea API functions
+
+### getEventByCollection()
+event_type works just for sale, transfer, cancel and redemption - all and order had "internal server error"
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,11 @@ import opensea
 import json
 
 address: str = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"
+collection_slug: str = "warriors-of-aradena"
+cursor = "LWV2ZW50X3RpbWVzdGFtcD0yMDIzLTA4LTEyKzIxJTNBNDklM0EyNy4zNDUyNTAmLWV2ZW50X3R5cGU9Y2FuY2VsbGVkJi1waz0xNDI4OTUyNTQyNA=="
 
-response = json.loads(opensea.getEventsByNFT(address, chain="ethereum", identifier= "1"))
-print(response["asset_events"])
+response = json.loads(opensea.getEventsByCollection(collection_slug, event_type = "cancel",next = cursor))
+# print(len(response["asset_events"]))
+print(response)
+
 
